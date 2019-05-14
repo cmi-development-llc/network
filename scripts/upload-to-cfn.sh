b57b7fd91d19c58f4c4c19128f88248fd477a75d
Bryan Pedlar <pedlar.bryan@gmail.com>
2019-05-14 13:15:25 -0400

    streamlined configuration

---
+++ scripts/upload-to-cfn.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+aws s3 cp cloudformation/ s3://cmidev/ --recursive
+aws cloudformation update-stack --stack-name website \
+    --template-body file://cloudformation/website-master.yml \
+    --parameters file://cloudformation/website-master.parameters.json
+aws cloudformation wait stack-update-complete --stack-name website
+echo "success"
